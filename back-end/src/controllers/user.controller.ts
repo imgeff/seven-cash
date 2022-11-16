3e41b6f4c30fe1c155509ce9506c6b6b1484b14b
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-16 18:51:40 -0300

    :sparkles: feat: Add user register controller

---
+++ back-end/src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import 'express-async-errors';
+import { Request, Response } from 'express';
+import { IUserEntityRequest } from '../database/entities/IUser.entity';
+import { IUserService } from '../services/interfaces/IUser.service';
+import { IUserController } from './interfaces/IUser.controller';
+import Token from '../helpers/token';
+import { StatusCodes } from 'http-status-codes';
+
+export class UserController implements IUserController {
+  private _userService: IUserService;
+
+  constructor(userService: IUserService) {
+    this._userService = userService;
+  }
+
+  public async register(req: Request, res: Response): Promise<Response> {
+    const { username, password }: IUserEntityRequest = req.body;
+
+    const userCreated = await this._userService.register({ username, password });
+    const token = Token.generate(userCreated);
+
+    return res.status(StatusCodes.CREATED).json({ ...userCreated, token });
+  }
+  
+}
