f392b311a79edbdaec47982990fa25953cb24b6f
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-17 15:08:04 -0300

    :sparkles: feat: Add account transactions controller

--- back-end/src/controllers/interfaces/IAccount.controller.ts
+++ back-end/src/controllers/interfaces/IAccount.controller.ts
@@ -2,4 +2,5 @@ import { Request, Response } from "express";
 
 export interface IAccountController {
   getBalance(req: Request, res: Response): Promise<Response>;
+  getTransactions(req: Request, res: Response): Promise<Response>;
 }
