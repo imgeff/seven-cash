3ae856df95c792f9dc53faef1876b22b815b6441
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-17 12:27:32 -0300

    :sparkles: feat: Add update  account service method

--- back-end/src/services/account.service.ts
+++ back-end/src/services/account.service.ts
@@ -1,5 +1,6 @@
 import { Database } from "../database";
 import { IAccount } from "../database/entities/IAccount.entity";
+import { ThrowError } from "../helpers/error";
 import { IAccountService } from "./interfaces/IAccount.service";
 
 export class AccountService implements IAccountService {
@@ -13,5 +14,24 @@ export class AccountService implements IAccountService {
     const account = await this._database.account.create({ data: {} });
     return account;
   }
-  
+
+  public async getBalance(id: number): Promise<number | void> {
+    const account = await this._database.account.findFirst({ where: { id } });
+
+    if (account) {
+      return account.balance;
+    }
+
+    ThrowError.NotFound('Account');
+  }
+
+  public async update({ id, balance }: IAccount): Promise<IAccount> {
+    const accountUpdated = await this._database.account.update({ 
+      where: { id },
+      data: { balance }
+    });
+
+    return accountUpdated;
+  }
+
 }
