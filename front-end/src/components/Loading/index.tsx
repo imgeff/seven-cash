77764b1ad18145114a8d8115bc7954bbcdee680d
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-19 00:08:42 -0300

    :sparkles: feat: Add Loading component

---
+++ front-end/src/components/Loading/index.tsx
@@ -0,0 +1,15 @@
+import { ThreeDots } from "react-loader-spinner";
+import { ILoadingProps } from "./interfaces/ILoadingProps";
+
+export function Loading({ size, color }: ILoadingProps) {
+	return (
+		<ThreeDots
+			height={size}
+			width={size}
+			radius="9"
+			color={color}
+			ariaLabel="three-dots-loading"
+			visible={true}
+		/>
+	);
+}
