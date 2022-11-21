0086a17f336a2e2d7713bff9126d3c44d73db7fc
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-20 23:36:33 -0300

    :sparkles: feat: Add transaction requests

--- front-end/src/services/requests/Account/index.ts
+++ front-end/src/services/requests/Account/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { getItemLocalStorage } from "../../../helpers/Localstorage";
 import { IUserLocalStorage } from "../../../helpers/Localstorage/interfaces/IUserLocalStorage";
+import { IRequestTransactionsResponse } from "./interfaces/IRequestTransactionsResponse";
 
 const api = axios.create({
 	baseURL: `${process.env.REACT_APP_BASE_URL}/account`,
@@ -11,6 +12,32 @@ export async function requestBalance() {
 		const user = getItemLocalStorage("user") as IUserLocalStorage;
 		api.defaults.headers.common.Authorization = user.token;
 		const { data } = await api.get("/balance");
+		return data;
+	} catch (error: any) {
+		console.error(error);
+		return error.response?.data.message || error.message;
+	}
+}
+
+export async function requestTransactions():
+	Promise<IRequestTransactionsResponse | string> {
+	try {
+		const user = getItemLocalStorage("user") as IUserLocalStorage;
+		api.defaults.headers.common.Authorization = user.token;
+		const { data } = await api.get("/transactions");
+		console.log(data);
+		return data;
+	} catch (error: any) {
+		console.error(error);
+		return error.response?.data.message || error.message;
+	}
+}
+
+export async function requestFilterTransactions(filter: string) {
+	try {
+		const user = getItemLocalStorage("user") as IUserLocalStorage;
+		api.defaults.headers.common.Authorization = user.token;
+		const { data } = await api.get(`/transactions/${filter}`);
 		return data;
 	} catch (error: any) {
 		console.error(error);
