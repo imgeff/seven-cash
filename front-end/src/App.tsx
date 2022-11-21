96e6fa52c2efec6f34b383ef2370eaeb54556b8c
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-20 23:48:02 -0300

    :sparkles: feat: Implements main page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import { GlobalContextProvider } from "./contexts/Global/GlobalContextProvider";
 import { routes } from "./routes";
 
 function App() {
 	return (
-		<Routes>
-			{ routes.map(({ path, element }) => (
-				<Route key={path} path={path} element={element}/>
-			))}
-		</Routes>
+		<GlobalContextProvider>
+			<Routes>
+				{ routes.map(({ path, element }) => (
+					<Route key={path} path={path} element={element}/>
+				))}
+			</Routes>
+		</GlobalContextProvider>
 	);
 }
 
